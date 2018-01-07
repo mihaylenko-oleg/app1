806b67a04df0088c14617c07a079db4a15a5043f
mihaylenko-oleg <oleg89636405099@gmail.com>
2018-01-07 02:13:42 +0200

    Second commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -12,3 +12,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+//hello
